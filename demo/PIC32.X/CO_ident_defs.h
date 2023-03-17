ae81d7216b93aff2eeb70e8695c406f79459b465
Janez <janez.paternoster@siol.net>
2023-03-17 18:34:53 +0100

    demo: update demoDevice.xdd and files with the latest CANopenEditor (v4.1-1)

--- demo/PIC32.X/CO_ident_defs.h
+++ demo/PIC32.X/CO_ident_defs.h
@@ -14,10 +14,10 @@
  * version. It has form `"<tag>-<n>-g<commit>[-dirty]"`, where `<tag>` is
  * name of the last tag, `<n>` is number of commits above the tag, `g` is for
  * git, `<commit>` is commit ID and `-dirty` shows, if git is not clean. */
-#define CO_SW_VERSION ""
+#define CO_SW_VERSION "---"
 
 /* Identity, OD entry 0x1018 */
 #define CO_IDENTITY_VENDOR_ID 0x00000000
 #define CO_IDENTITY_PRODUCT_CODE 0x00000001
-#define CO_IDENTITY_REVISION_NUMBER 0x00000002
+#define CO_IDENTITY_REVISION_NUMBER 0x00000000
 #define CO_IDENTITY_SERIAL_NUMBER 0x00000003
